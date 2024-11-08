229880941b24968c7b2021a681eb72da09c63417
Afridul369 <afridul.dev@gmail.com>
2024-11-08 22:35:07 +0600

    Shop Part Added

--- src/componants/pages/Shop.jsx
+++ src/componants/pages/Shop.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Navbar from '../Navbar'
+import Category from '../Category'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <>
+    <Navbar />
+    <Category />
+    
+    </>
   )
 }
 
