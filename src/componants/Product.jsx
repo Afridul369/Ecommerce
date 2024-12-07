c6ec51fcb45c4c914e0ba16b4533fd9398441a71
Afridul369 <afridul.dev@gmail.com>
2024-12-07 14:57:14 +0600

    DummyJson API Added

--- src/componants/Product.jsx
+++ src/componants/Product.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Badge from './Badge'
 import Price from './Price'
 import Products from '../componants/Products'
@@ -6,14 +6,14 @@ import Cat from '../assets/cat.png'
 import Watch from '../assets/watch.png'
 import Clock from '../assets/clock.png'
 import Basket from '../assets/basket.png'
-import Bag1 from '../assets/bag1.png'
-import Bag2 from '../assets/bag2.png'
-import Puta from '../assets/puta.png'
-import Jar from '../assets/jar.png'
 import Cap from '../assets/cap.png'
 import Table from '../assets/table.png'
 import Headphone from '../assets/headphone.png'
 import Sunglass from '../assets/sunglass.png'
+import Bag1 from '../assets/bag1.png'
+import Bag2 from '../assets/bag2.png'
+import Puta from '../assets/puta.png'
+import Jar from '../assets/jar.png'
 import "slick-carousel/slick/slick.css";
 import Slider from "react-slick";
 import Container from './Container'
@@ -21,9 +21,17 @@ import PrevArrow from './PrevArrow'
 import NextArrow from './NextArrow'
 import Text from './Text'
 import Flex from './Flex'
+import axios from 'axios'
 
 const Product = () => {
-
+  let [all,setAll] = useState([])
+  useEffect(()=>{
+  async function all(){
+      let data = await axios.get('https://dummyjson.com/products')
+      setAll(data.data.products)
+    }
+    all();
+    },[])
   var settings = {
        
     infinite: true,
@@ -31,7 +39,9 @@ const Product = () => {
     slidesToShow: 4,
     slidesToScroll: 1,
     nextArrow: <NextArrow/>,
-    prevArrow: <PrevArrow/>
+    prevArrow: <PrevArrow/>,
+    autoplay: true,
+    autoplayspeed:1000,
   };
  
   return (
@@ -39,44 +49,30 @@ const Product = () => {
  <div className="mb-40">
  <Container>
   <Text text={'New Arrivals'} as='p' className='text-hoverheadeingC text-[39px] font-bold font-dm mb-10'/>
- <Slider {...settings}>
-    <Products imgSrc={Clock}
-    text1={'Basic Crew Neck Tee'}
-    text2={'$44.00'} text3={'10%'} text4={'Add to Wish List'} text5={'Compare'} text6={'Add to Cart'} text7={'Black'}
-    />
-    <Products imgSrc={Watch}
-    text1={'Basic Crew Neck Tee'}
-    text2={'$44.00'} text3={'New'} text4={'Add to Wish List'} text5={'Compare'} text6={'Add to Cart'} text7={'Black'}
-    />
-    <Products imgSrc={Basket}
-    text1={'Basic Crew Neck Tee'}
-    text2={'$44.00'} text3={'New'} text4={'Add to Wish List'} text5={'Compare'} text6={'Add to Cart'} text7={'Black'}
-    />
-    <Products imgSrc={Cat}
-    text1={'Basic Crew Neck Tee'}
-    text2={'$44.00'} text3={'New'} text4={'Add to Wish List'} text5={'Compare'} text6={'Add to Cart'} text7={'Black'}
-    />
-    <Products imgSrc={Cap}
-    text1={'Basic Crew Neck Tee'}
-    text2={'$44.00'} text3={'New'} text4={'Add to Wish List'} text5={'Compare'} text6={'Add to Cart'} text7={'Black'}
-    />
-    <Products imgSrc={Table}
-    text1={'Basic Crew Neck Tee'}
-    text2={'$44.00'} text3={'New'} text4={'Add to Wish List'} text5={'Compare'} text6={'Add to Cart'} text7={'Black'}
-    />
-    <Products imgSrc={Headphone}
-    text1={'Basic Crew Neck Tee'}
-    text2={'$44.00'} text3={'New'} text4={'Add to Wish List'} text5={'Compare'} text6={'Add to Cart'} text7={'Black'}
-    />
-    <Products imgSrc={Sunglass}
-    text1={'Basic Crew Neck Tee'}
-    text2={'$44.00'} text3={'New'} text4={'Add to Wish List'} text5={'Compare'} text6={'Add to Cart'} text7={'Black'}
-    />
-    </Slider>
+  <Slider {...settings}>
+            {all.map((item)=>
+              <div className="w-full " >
+                <Products imgSrc={item.images}
+                text1={item.title}
+                text2={`$ ${item.price}`} text3={`${item.discountPercentage}%`} text4={'Add to Wish List'} text5={'Compare'} text6={'Add to Cart'} text7={item.category}
+                />
+              </div>
+            )}
+        </Slider>
+  <Slider {...settings}>
+            {all.map((item)=>
+              <div className="w-full " >
+                <Products imgSrc={item.images}
+                text1={item.title}
+                text2={`$ ${item.price}`} text3={`${item.discountPercentage}%`} text4={'Add to Wish List'} text5={'Compare'} text6={'Add to Cart'} text7={item.category}
+                />
+              </div>
+            )}
+        </Slider>
  </Container>
  </div>
  
- <div className="mb-28">
+ {/* <div className="mb-28">
  <Container>
  <Flex >
     <Products imgSrc={Jar}
@@ -97,7 +93,7 @@ const Product = () => {
     />
     </Flex>
  </Container>
- </div>
+ </div> */}
  
  
     </>
